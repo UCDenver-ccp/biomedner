cf27b533fe7abf9e786bea96daa154d2f316904e
Chris Roeder <Chris.Roeder@ucdenver.edu>
2013-05-30 11:55:33 -0600

    better error handling, can choose batching or not in the MySQLDatabaseBuilder

--- src/bioner/normalization/data/database/MySQLDatabaseBuilder.java
+++ src/bioner/normalization/data/database/MySQLDatabaseBuilder.java
@@ -15,36 +15,36 @@ public class MySQLDatabaseBuilder {
 	
 	private Connection conn=null;
 	
-	public MySQLDatabaseBuilder()
-	{
-		try{
+	public MySQLDatabaseBuilder() {
+		try {
 			//The new Instance() call is a work around for some
 			//broken Java implementations
 			Class.forName(DatabaseConfig.DATABASE_DRIVER_CLASS).newInstance();
-		}catch(Exception ex){
-			//handle the error
+		} 
+		catch(Exception ex) {
 			ex.printStackTrace();
 			throw new RuntimeException(ex);
 		}
 		DatabaseConfig.ReadConfigFile();
 	}
-	public void connect()
-	{
+
+	public void connect() {
 		try {
+			System.out.println("connecting to:" + DatabaseConfig.DATABASE_NAME + " " + DatabaseConfig.DATABASE_USERNAME);
+			System.out.println("will be inserting into:" + DatabaseConfig.DATABASE_GENEINFO_TABLE_NAME);
 			conn = DriverManager.getConnection("jdbc:"+DatabaseConfig.DATABASE_DRIVER_NAME+"://"+DatabaseConfig.DATABASE_HOST+"/"+DatabaseConfig.DATABASE_NAME+"?"+
 												"user="+DatabaseConfig.DATABASE_USERNAME+"&password="+DatabaseConfig.DATABASE_PASSWORD);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 	}
+
 	public void close()
 	{
 		try {
 			conn.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
@@ -76,11 +76,10 @@ public class MySQLDatabaseBuilder {
 			stmt.execute("drop table "+tableName);
 			}catch(SQLException e)
 			{
-				System.out.println("Table doesn't exist! Create new table");
+				System.out.println("INFO: Table doesn't exist! Create new table");
 			}
 			stmt.execute(sql);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
@@ -88,6 +87,10 @@ public class MySQLDatabaseBuilder {
 	}
 	public void importData(String filename)
 	{
+
+		final boolean batch=true;
+		final int batchSize = 1000;
+
 		String tableName = DatabaseConfig.DATABASE_GENEINFO_TABLE_NAME;
 		StringBuilder sql = null;
 		try {
@@ -97,64 +100,80 @@ public class MySQLDatabaseBuilder {
 			int bogusNum=0;
 			Statement stmt = conn.createStatement();
 			sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
-			while((line=freader.readLine()) != null)
-			{
-				if(line.length()<=0 || line.startsWith("#")) continue;
+			while ((line=freader.readLine()) != null) {
+				if (line.length()<=0 || line.startsWith("#"))  {
+					continue;
+				}
 				num++;
 				
 				line = line.replaceAll("\\\\", "\\\\\\\\");
 				line = line.replaceAll("\\'", "\\\\'");
 				String[] parts = line.split("\\t+");
 				sql.append("(");
-				for(int i=0; i<parts.length; i++)
-				{
-					if(i>0) sql.append(",");
-					if(!parts[i].equals("-"))
-					{
+
+
+				for (int i=0; i<parts.length; i++) {
+					if (i>0) {
+						sql.append(",");
+					}
+					if (!parts[i].equals("-")) {
 						//sql.append("'"+parts[i]+"'");
 						// http://stackoverflow.com/questions/12316953/insert-varchar-with-single-quotes-in-postgresql
 						sql.append("E'"+parts[i]+"'");
 					}
-					else
-					{
+					else {
 						sql.append("null");
 					}
 				}
-				//sql.append("),");
-				sql.append(");");
+
 
 				// BATCHES
-				if(num%1000==0) 
-				{
-					System.err.println("Adding #"+num+" Record");
-					
-				//	if(sql.charAt(sql.length()-1)==',')
-				//	{
-				//		sql.deleteCharAt(sql.length()-1);
-				//	}
-				//	stmt.execute(sql.toString());
-				//	sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
-				}
-		
-				// ONE-by-ONE
+				if (num % batchSize==0) {
+					System.err.println("Adding #" + num + " Records");
+				}	
+
 				try {
-					stmt.execute(sql.toString());
-					sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
+					if (batch) {	
+						sql.append("),");
+						if (num % batchSize==0) {
+							if (sql.charAt(sql.length()-1)==',') {
+								sql.deleteCharAt(sql.length()-1);
+							}
+							stmt.execute(sql.toString());
+							sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
+						}
+					}
+					else {
+						sql.append(");");
+						stmt.execute(sql.toString());
+						sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
+					}
 				}
 				catch (Exception x) {
 					bogusNum++;
 					System.err.println("\nERROR:" + x);
 					System.err.println("did not insert:" + bogusNum + "/" +  num + " "  + sql);
+					System.err.println("...continuing to process remaineder");
 					sql = new StringBuilder("INSERT INTO "+tableName + " VALUES ");
 				}
-				
 			}
 			
-			//if(sql.charAt(sql.length()-1)==',')
-			//{
-			//	sql.deleteCharAt(sql.length()-1);
-			//}
-			//stmt.execute(sql.toString());
+	
+			if (batch) {
+				if (sql.charAt(sql.length()-1)==',') {
+					sql.deleteCharAt(sql.length()-1);
+				}
+				try {
+					stmt.execute(sql.toString());
+				}
+				catch (Exception x) {
+					bogusNum++;
+					System.err.println("\nERROR:" + x);
+					System.err.println("did not insert:" + bogusNum + "/" +  num + " "  + sql);
+					System.err.println("...continuing to finish process despite errors.");
+				}
+			}
+
 			System.out.println("Done Adding #"+num+" Record");
 			freader.close();
 		} catch (FileNotFoundException e) {
@@ -165,15 +184,19 @@ public class MySQLDatabaseBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException(e);
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) {
 			System.out.println("\nERROR with STATEMENT: " + sql.toString());
 			e.printStackTrace();
 			throw new RuntimeException(e);
-		} catch (Exception e) {
+		} 
+/***
+catch (Exception e) {
 			System.out.println("\n ERROR with STATEMENT: " + sql.toString());
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}	
+***/
 	}
 	
 	/**
